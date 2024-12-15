457f59372c5df8f3c005eaa163e36a100bc53329
Alloud <palikarya22@hotmail.com>
2024-12-15 12:51:31 +0300

    UpdateV6

---
+++ Assets/Scripts/HighLighter.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighLighter : MonoBehaviour
+{
+    [SerializeField] public GameObject Icons;
+    private SpriteRenderer spriteRenderer; // SpriteRenderer bileþeni
+    private Color originalColor; // Orijinal rengi
+    [SerializeField] private Color highlightColor = new Color(1f, 1f, 0.5f); // Hafif sarý renk
+
+    void Start()
+    {
+        // SpriteRenderer bileþenini al ve orijinal rengi sakla
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            originalColor = spriteRenderer.color;
+        }
+        else
+        {
+            Debug.LogError("SpriteRenderer bileþeni bulunamadý. Lütfen bu script'i bir SpriteRenderer içeren nesneye ekleyin.");
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        // Fare üzerine geldiðinde rengi deðiþtir
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.color = highlightColor;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        // Fare nesneden ayrýldýðýnda orijinal rengine dön
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.color = originalColor;
+        }
+    }
+}
